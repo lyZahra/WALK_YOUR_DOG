e81105b540d7fb1572c90bf5337c5f2399c134a8
Christopher Grant <christopher.ryan.grant@gmail.com>
2023-06-09 14:27:48 +0200

    updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!, only: :home
 
   def home
     @pets = Pet.all
